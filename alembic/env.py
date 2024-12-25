18dda7b46205004123e5ad47bce7f81988ac814f
Mayank Ranghadiya <mayankranghadia54154@gmail.com>
2024-12-25 15:13:42 +0530

    fix: alembic target

--- alembic/env.py
+++ alembic/env.py
@@ -5,8 +5,7 @@ from sqlalchemy import pool
 
 from alembic import context
 
-from khazana.core.utils import DBBaseModel
-from khazana.core.database import DATABASE_URL
+from khazana.core.database import DATABASE_URL, DBBaseModel
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
