81d957d9143a13108c8c422bb8082e690e8d8bec
Mayank Ranghadiya <mayankranghadia54154@gmail.com>
2024-12-27 14:50:25 +0530

    fix: new endpoint for user data

--- khazana/transactions/apis/dashboard.py
+++ khazana/transactions/apis/dashboard.py
@@ -22,7 +22,7 @@ router = APIRouter(prefix="/dashboard", tags=["Transaction Dashboard"])
     description="Get dashboard data.",
     response_model=dict,
 )
-def get_dashboard_data(
+def get_dashboard_data_by_username(
     username: str,
     db: Session = Depends(get_db),
     user: UserDB = Security(
@@ -35,6 +35,91 @@ def get_dashboard_data(
     """
     requested_user = (
         db.query(UserDB).filter(UserDB.username == username).first()
+    )
+    if not requested_user:
+        raise HTTPException(status_code=404, detail="User not found")
+
+    transactions = (
+        db.query(
+            TransactionDB.transactionDate,
+            TransactionDB.amount,
+        )
+        .filter(
+            TransactionDB.userId == requested_user.id,
+            or_(
+                TransactionDB.transactionType == TransactionType.income.value,
+                TransactionDB.transactionType == TransactionType.expense.value,
+            ),
+        )
+        .all()
+    )
+    total_savings_month_wise = {}
+    for key, transactions in groupby(
+        transactions,
+        key=lambda x: [
+            str(x.transactionDate.year),
+            str(x.transactionDate.month),
+        ],
+    ):
+        total_savings_month_wise["-".join(key)] = sum(
+            transaction.amount for transaction in transactions
+        )
+
+    expenses = (
+        db.query(
+            TransactionDB.transactionDate,
+            TransactionDB.amount,
+        )
+        .filter(
+            TransactionDB.userId == requested_user.id,
+            TransactionDB.transactionType == TransactionType.expense.value,
+        )
+        .all()
+    )
+    monthly_expenses = {}
+    for key, expenses in groupby(
+        expenses,
+        key=lambda x: [
+            str(x.transactionDate.year),
+            str(x.transactionDate.month),
+        ],
+    ):
+        monthly_expenses["-".join(key)] = sum(
+            expense.amount for expense in expenses
+        )
+    investment_growth = (
+        db.query(TransactionDB.category, sa_func.sum(TransactionDB.amount))
+        .filter(
+            TransactionDB.userId == requested_user.id,
+            TransactionDB.transactionType == TransactionType.investment.value,
+        )
+        .group_by(TransactionDB.category)
+        .all()
+    )
+    return {
+        "totalSavings": total_savings_month_wise,
+        "monthlyExpenses": monthly_expenses,
+        "investmentGrowth": dict(investment_growth),
+    }
+
+
+@router.get(
+    "",
+    description="Get dashboard data.",
+    response_model=dict,
+)
+def get_dashboard_data(
+    db: Session = Depends(get_db),
+    user: UserDB = Security(
+        get_current_user, scopes=["admin", "transaction_read"]
+    ),
+) -> dict:
+    """Get aggregate data.
+
+    For total savings, monthly expenses, and investment growth by user.
+    """
+    requested_user = (
+        db.query(UserDB).filter(UserDB.username == user.username).first()
     )
     if not requested_user:
         raise HTTPException(status_code=404, detail="User not found")
